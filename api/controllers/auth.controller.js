b481f8f9e40580d2a4a7af8ab7d1082c73bbdfb7
Akanshu Aich <aichakanshu2003@gmail.com>
2024-01-28 23:29:15 +0530

    Complete signup page functionality

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req , res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword});
+    try{
+        await newUser.save();
+        res.status(201).json("User created successfully");
+    } catch(error){
+        next(error);
+    }
+}    
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 
@@ -20,5 +21,18 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
     }
 );
-
+app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+//creating middleware to handle error
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
