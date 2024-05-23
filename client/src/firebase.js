303374dc41fa26461c00fcedf44b233057473c3c
Akanshu Aich <b221008@iiit-bh.ac.in>
2024-05-24 03:30:10 +0530

    Change in firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import VITE_FIREBASE_API_KEY from '../../firebasekey';
+import VITE_FIREBASE_API_KEY from '../../firebasekey.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
