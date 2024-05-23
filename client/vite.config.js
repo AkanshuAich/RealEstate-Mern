b0a42f77568b27f222a803b27d44e01995183754
Akanshu Aich <b221008@iiit-bh.ac.in>
2024-05-24 01:03:22 +0530

    changes in client file and commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +11,6 @@ export default defineConfig({
       },
     },
   },
+
   plugins: [react()],
-})
+});
