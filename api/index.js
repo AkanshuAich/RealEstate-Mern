42163151dc308fb83373693c61144b8ddf7ef26f
Akanshu Aich <b221008@iiit-bh.ac.in>
2024-05-24 03:45:57 +0530

    final commit

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import MONGO from '../mongo.js';
 import path from 'path';
 dotenv.config();
 
